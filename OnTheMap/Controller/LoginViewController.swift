933e2fd5002f218bdd37fb29ce46799f4481f2ed
Wolfgang Sigel <wsigel@web.de>
2020-04-13 12:26:10 +0200

    Add controller file for Map view

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class LoginViewController: ViewController {
+class LoginViewController: UIViewController {
     
     
     
---
+++ OnTheMap/Controller/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Wolfgang Sigel on 13.04.20.
+//  Copyright © 2020 Wolfgang Sigel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+}
