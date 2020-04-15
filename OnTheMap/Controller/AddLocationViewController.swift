2f0667e55e06b91389fb72e5782c280a59177141
Wolfgang Sigel <wsigel@web.de>
2020-04-15 18:57:26 +0200

    Add navigation bar items & information posting view

---
+++ OnTheMap/Controller/AddLocationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Wolfgang Sigel on 15.04.20.
+//  Copyright © 2020 Wolfgang Sigel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
