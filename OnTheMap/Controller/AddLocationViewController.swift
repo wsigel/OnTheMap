15babce367284d073e2504ab10aa839abcc58802
Wolfgang Sigel <wsigel@web.de>
2020-04-19 15:20:37 +0200

    Add geocoding for address string

--- OnTheMap/Controller/AddLocationViewController.swift
+++ OnTheMap/Controller/AddLocationViewController.swift
@@ -8,10 +8,29 @@
 
 import Foundation
 import UIKit
+import MapKit
+import CoreLocation
 
 class AddLocationViewController: UIViewController {
     
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    
     @IBAction func cancelButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func findLocationButtonTapped(_ sender: Any) {
+        guard let locationText = self.locationTextField.text else {
+            return
+        }
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(locationText) { (placemarks, error) in
+            if let placemark = placemarks?.first {
+                if let coordinates: CLLocationCoordinate2D = placemark.location?.coordinate {
+                    print("Breite \(coordinates.longitude) / Länge \(coordinates.latitude)")
+                }
+            }
+        }
     }
 }
